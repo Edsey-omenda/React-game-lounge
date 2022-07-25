beaaf22e65d7af4ba65b19beaf159540b5dddc26
Edsey omenda <omendajames0317@gmail.com>
2022-07-25 20:43:30 +0300

    creates the Navbar component and defines the Navbar function

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar(){
+    return (
+        <nav className='navbar bg -dark'>
+            < div className='container-fluid'>
+                <ul className='navbar-nav'>
+                    
+                </ul>
+            </div>
+        </nav>
+    )
+}
