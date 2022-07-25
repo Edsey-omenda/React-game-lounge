e7efb164716033540aa6e737e1729fd7a6f46513
Edsey omenda <omendajames0317@gmail.com>
2022-07-26 01:26:08 +0300

    creates and exports the game description function for giving details about each game

---
+++ src/components/GameDescription.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+
+
+export default function GameDescription(){
+    const[image, setImage] = useState("")
+    const[title, setTitle] = useState("")
+    const[category, setCategory] = useState("")
+    const[instructions, setInstructions] = useState("")
+
+}
