26f143111cd1547c1111d41b8aba37a23d5a32de
Edsey omenda <omendajames0317@gmail.com>
2022-07-26 00:37:34 +0300

    creates a badge for giving more description about game on the card

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-export default function GameCard({thumbnail, title}){
+export default function GameCard({thumbnail, title, gameId}){
     return(
         <div className='card' style={{width : 18 + 'rem'}}>
             <img src={thumbnail} className="card-img-top" alt={title}
             />
             <div className='card-body'>
-                
+            <h5 className="card-title">{title}</h5>
+                <Link to={"/meal/"+gameId} className="btn btn-primary">Game Description</Link>
             </div>
         </div>
     )
