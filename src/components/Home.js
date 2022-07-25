70412f076e61fddb93d6c90a53f39900707e608a
Edsey omenda <omendajames0317@gmail.com>
2022-07-25 23:47:45 +0300

    calls useState to set the games on the dom

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function HOme(){
+    const [games, setGames] = useState([])
 
     useEffect(() => {
         fetch("https://www.freetogame.com/api/games?category=3d")
         .then(response => response.json())
         .then((data) => {
+            setGames(data.games)
           console.log(data)
         })
       }, [])
