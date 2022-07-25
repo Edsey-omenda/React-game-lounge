50aac9095bff3d6f92495ad2ad9e73fe58a6c09e
Edsey omenda <omendajames0317@gmail.com>
2022-07-26 00:01:03 +0300

    passes thumbnail and title as props in the component

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-export default function GameCard(){
+export default function GameCard({thumbnail, title}){
     return(
-        <div className='card' style={{width : 16 + 'rem'}}></div>
+        <div className='card' style={{width : 16 + 'rem'}}>
+            <img src={thumbnail} className="card-img-top" alt={title}
+            />
+            <div className='card-body'>
+                
+            </div>
+        </div>
     )
 }
