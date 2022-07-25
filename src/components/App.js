776d8e840bf74be791cf62b783bfa12a55d93d62
Edsey omenda <omendajames0317@gmail.com>
2022-07-25 23:37:17 +0300

    uses routes and route to render the home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
        <div className="App">
     <Navbar />
-    <Home />
     </div>
+    <Routes>
+      <Route  exact path="/" element={<Home />}></Route>
+      <Route  exact path="/games" eleme></Route>
+    </Routes>
     </Router>
    
   );
