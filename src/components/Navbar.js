0f56f2c7bdd475d5eb6c02f5ca0d8fb1d8e0e138
Edsey omenda <omendajames0317@gmail.com>
2022-07-25 20:55:20 +0300

    imports link from react router dom to render the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return (
         <nav className='navbar bg -dark'>
             < div className='container-fluid'>
                 <ul className='navbar-nav'>
-                    
+                    < li className='nav-item'>
+                        <Link className='nav-link' to="/">Home </Link>
+                    </li>
                 </ul>
             </div>
         </nav>
