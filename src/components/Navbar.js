cf2df04a9abf35248418a96eeed085a52776b463
Edsey omenda <omendajames0317@gmail.com>
2022-07-25 21:11:17 +0300

    adds the games link to the navbar of the home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return (
-        <nav className='navbar bg -blue'>
+        <nav className='navbar bg -dark'>
             < div className='container-fluid'>
                 <ul className='navbar-nav'>
                     < li className='nav-item'>
                         <Link className='nav-link' to="/">Home </Link>
+                    </li>
+                    < li className='nav-item'>
+                        <Link className='nav-link' to="/">Games </Link>
                     </li>
                 </ul>
             </div>
