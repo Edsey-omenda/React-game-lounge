2677c9e0015561a7e71fb417ea36e88e0cb22223
Edsey omenda <omendajames0317@gmail.com>
2022-07-25 21:41:19 +0300

    fetches the 3-D games and succesfully returns a fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+
+const ThreeeDGames = "https://www.freetogame.com/api/games?category=3d";
 
 export default function HOme(){
+const [games, setGames] = useState([]);
+ const myGames = () =>{
+    fetch(ThreeeDGames)
+    .then((resposnse) => Response.json())
+ }
+
+
     return(
-        <div></div>
+        <div>
+           
+        </div>
     )
 }
