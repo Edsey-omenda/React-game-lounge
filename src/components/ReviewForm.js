b36ca149550f355d0be587a4edecb508a4e39028
Edsey omenda <omendajames0317@gmail.com>
2022-07-26 16:50:47 +0300

    creates a form input for posting of game review

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -4,7 +4,13 @@ export default function Post(){
 
     return(
         <div>
-            
+              <form onSubmit={handleSubmit}>
+        <input type="text"   />
+        <input type="text"   />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Game review</h3>
+      
         </div>
     )
 }
