2598d5dd3be42494c21a509b738ae28a14e66e45
Edsey omenda <omendajames0317@gmail.com>
2022-07-26 20:42:13 +0300

    creates a local db.json

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(){
     return (
         <nav className='navbar navbar-expand-lg bg-light'>
             < div className='container-fluid'>
-                <ul className='navbar-nav'>
+                <ul className='navbar-nav col-white'>
                     < li className='nav-item'>
                         <Link className='nav-link' to="/">Home </Link>
                     </li>
