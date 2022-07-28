84972bc1ecabf804bb0b4f581f90d941e2e936fa
Edsey omenda <omendajames0317@gmail.com>
2022-07-28 14:38:36 +0300

    updates the developer component to display details of the developer

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import pic from "../Assets/20201231_153057.jpg";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {faLinkedin,faTwitter,faGithub} from "@fortawesome/free-brands-svg-icons";
 
 
 export default function Developer(){
   
 
     return(
-      <div className="text-bg-info p-3">
-        <div className='container'>Developer!
+      <div className="p-3 mb-2 bg-info text-dark">
         <center>
-        <div className='row' >
-          <h2>Welcome to React Game Lounge!</h2>
-         
-          </div>
-        </center>
-        </div>
+        <div className='container'>Developer!
+        <h1>Contact Details</h1>
+        <h2>Hi, i'm James, a Software Engineer at Flatiron/Moringa School</h2>
+        <p className="p">
+        Email:<a href="omendajames0317@gmail.com">omendajames0317@gmail</a><br></br>
+        Github:<a href="https://github.com/Edsey-omenda">Edsey-omenda
+      </a>
+      <br></br>
+      Linkedin:<a href="https://www.linkedin.com/in/james-omenda-b5352418a?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMjS%2BicXTSM%2B0P8FftAs2kw%3D%3D">James Omenda</a>
+      </p>
+      </div>
+      <img src={pic} class="rounded-circle" alt="Wuod Nyagoro" style={{width : 25 + 'rem', height : 25 + 'rem'}}/>
+      </center>
         </div>
     )
 }
